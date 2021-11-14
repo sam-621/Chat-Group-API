2bb8f19c64b725829ac397bb4a05317dfe3e6a7d
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-11-14 14:11:33 -0700

    grapql config created

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-const hi: string = 'hi';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-console.log(hi);
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.start().then((res) => {
+  const app = express();
+  server.applyMiddleware({ app });
+
+  app.use('/', (req, res) => res.send('in home'));
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+});
