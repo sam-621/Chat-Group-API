e6e8b96cb16f6f10a12f9f94ebcea758a203fb9e
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-12 19:36:32 -0700

    se agrego el archiov secrets

--- scripts/secrets.js
+++ scripts/secrets.js
@@ -1,7 +1,9 @@
 const crypto = require('crypto');
 
 const apiKey = crypto.randomBytes(64).toString('hex');
+const tokenSecret = crypto.randomBytes(64).toString('hex');
 
 console.log('copy the output below and paste it in your env.local \n');
 
 console.log(`API_KEY=${apiKey}`);
+console.log(`JWT_SECRET=${tokenSecret}`);
