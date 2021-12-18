c6a45cf4db6832ca3a925f63a89bdc1181511dde
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-17 21:31:33 -0700

    add default values to env variables

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -5,11 +5,11 @@ dotenv.config({ path: path.resolve(process.cwd(), '.env.local') });
 
 //APP
 export const MODE = process.env.NODE_ENV;
-export const PORT = Number(process.env.PORT);
+export const PORT = Number(process.env.PORT) || 4000;
 
 //MONGO
 export const MONGO_DB_URI = process.env.MONGO_DB_URI;
 
 //JWT
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+export const JWT_SECRET = process.env.JWT_SECRET || 'secret';
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '15s';
